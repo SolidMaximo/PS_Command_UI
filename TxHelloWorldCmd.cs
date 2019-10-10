09199f84a1acdbbc31ce5bc011c935ecda520812
Solidmaximo <stevenlstafford@gmail.com>
2019-10-10 15:12:12 -0400

    First commit

---
+++ TxHelloWorldCmd.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tecnomatix.Engineering;
+
+namespace TrainingCommands
+{
+    public class TxHelloWorldCmd : TxButtonCommand
+    {
+        public override string Category
+        {
+            get
+            {
+                return StringTable.CATEGORY;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return StringTable.NAME;
+            }
+        }
+
+        public override void Execute(object cmdParams) //defines actual work done inside Process Simulate
+        {
+            //add message box-- need to reference windows.forms library 
+            MessageBox.Show("Hello World!", Name, MessageBoxButtons.OK, MessageBoxIcon.Information); 
+
+        }
+
+        public override string Bitmap
+        {
+            get
+            {
+                return "HelloWorld.bmp";
+            }
+        }
+        public override string LargeBitmap
+        {
+            get
+            {
+                return "HelloWorld.png";
+            }
+        }
+    }
+}
+
